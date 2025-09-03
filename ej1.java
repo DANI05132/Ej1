5498bdb87ab5d9815bf83a02868dba7826c5d61c
DANI05132 <weroarmendariz123@gmail.com>
2025-09-03 03:14:15 -0700

    ej1 java

---
+++ ej1.java
@@ -0,0 +1,9 @@
+public class ej1 {
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+        System.out.println("Elementos del arreglo:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("numeros[" + i + "] = " + numeros[i]);
+        }
+    }
+}
