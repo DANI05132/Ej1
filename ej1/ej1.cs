a4755a61e87bb35fa0948a2506052e1ede0a857c
DANI05132 <weroarmendariz123@gmail.com>
2025-09-03 03:15:14 -0700

    ej1 c#

---
+++ ej1/ej1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Ej1
+{
+    static void Main(string[] args)
+    {
+        int[] numeros = { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Elementos del arreglo:");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine($"numeros[{i}] = {numeros[i]}");
+        }
+    }
+}
