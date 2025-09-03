21490ce474a50024120bc762d8379421b6fcf4dd
DANI05132 <weroarmendariz123@gmail.com>
2025-09-03 03:14:34 -0700

    ej1 js

---
+++ ej1.js
@@ -0,0 +1,9 @@
+function main() {
+    let numeros = [10, 20, 30, 40, 50];
+    console.log("Elementos del arreglo:");
+    for (let i = 0; i < numeros.length; i++) {
+        console.log(`numeros[${i}] = ${numeros[i]}`);
+    }
+}
+
+main();
