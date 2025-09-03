a875df6885d1dafa0e0e8ca5af777bcd72f29f51
DANI05132 <weroarmendariz123@gmail.com>
2025-09-03 03:14:52 -0700

    ej1 python

---
+++ ej1.py
@@ -0,0 +1,8 @@
+def main():
+    numeros = [10, 20, 30, 40, 50]
+    print("Elementos del arreglo:")
+    for i in range(5):
+        print(f"numeros[{i}] = {numeros[i]}")
+
+if __name__ == "__main__":
+    main()
